2946ff4b90fb4b79a81fa35085f05501f8c62c97
Kunal Sindhwani <kunalsindhwani@hotmail.com>
2018-12-10 00:45:58 +0530

    Add files via upload

---
+++ src/main/java/com/example/demoAsync/DemoAsyncApplication.java
@@ -0,0 +1,12 @@
+package com.example.demoAsync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoAsyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoAsyncApplication.class, args);
+	}
+}
